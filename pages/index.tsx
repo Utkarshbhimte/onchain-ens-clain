7c86cb21693ab83147868978cb2e07e37d5e2836
abhikumar98 <abhishekkumar35962@gmail.com>
2022-09-26 14:36:49 +0530

    fix: 3d card

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,9 @@ import { useRouter } from "next/router";
 import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useAccount, useContract, useNetwork, useSigner } from "wagmi";
-import web3 from 'web3';
-import buildContractABI from '../abis/build.json';
+import web3 from "web3";
+import Card from "react-animated-3d-card";
+import ClaimedCard from "@/components/ClaimedCard";
 
 const preferredChainId = Number(process.env.NEXT_PUBLIC_CHAIN_ID);
 const Home: NextPage = () => {
@@ -18,11 +19,11 @@ const Home: NextPage = () => {
 	const { chain } = useNetwork();
 	const router = useRouter();
 
-	const { onWhiteList, proof } = useWhiteList(address)
+	const { onWhiteList, proof } = useWhiteList(address);
 
 	const [loading, setLoading] = useState(false);
-	const [name, setName] = useState('');
-	const [ensName, setEnsName] = useState('');
+	const [name, setName] = useState("");
+	const [ensName, setEnsName] = useState("");
 	const [hasClaimed, setHasClaimed] = useState(false);
 
 	// const signer = useSigner();
@@ -38,63 +39,70 @@ const Home: NextPage = () => {
 
 	const isLoginSuccessful = isValidChain && isConnected;
 
-	const claimEns = useCallback(async () => {
-		setLoading(true)
-		try {
-			const temp = await buildContract(address)?.merkleHash
-			const response = await buildContract(address)?.domainMap(name)
-			if(!!web3.utils.toAscii(response).replaceAll('\u0000', "").length) {
-				toast.error('ens domain not available!');
+	const claimEns = useCallback(
+		async (e: any) => {
+			e.preventDefault();
+			setLoading(true);
+			try {
+				const temp = await buildContract(address)?.merkleHash;
+				const response = await buildContract(address)?.domainMap(name);
+				if (
+					!!web3.utils.toAscii(response).replaceAll("\u0000", "")
+						.length
+				) {
+					toast.error("ens domain not available!");
+					setLoading(false);
+					return;
+				}
+				const tx = await buildContract(address)?.claimSubdomain(
+					name,
+					proof
+				);
+				await tx.wait();
+				setHasClaimed(true);
+				toast.success("Ens Claimed Successfully");
+				// router.push('/congratulations')
+				console.log("Transaction successfull");
+			} catch (error) {
+				console.error(error);
+			} finally {
 				setLoading(false);
-				return;
 			}
-			const tx = await buildContract(address)?.claimSubdomain(
-				name,
-				proof
-			)
-			await tx.wait();
-			setHasClaimed(true);
-			toast.success('Ens Claimed Successfully');
-			// router.push('/congratulations')
-			console.log('Transaction successfull')
-		} catch (error) {
-			console.error(error)
-		} finally {
-			setLoading(false)
-		}
-	}, [name, proof])
+		},
+		[name, proof]
+	);
 
 	const checkIfAlreadyExists = useCallback(async () => {
-		if(!address?.length || !isConnected) return
+		if (!address?.length || !isConnected) return;
 		setLoading(true);
 		try {
 			const hash = await buildContract()?.addressToHashmap(
 				address.toLowerCase()
-			)
-			setHasClaimed(!!web3.utils.toAscii(hash).replaceAll('\u0000', "").length)
-			if(!!web3.utils.toAscii(hash).replaceAll('\u0000', "").length){
-				const name = await buildContract()?.hashToDomainMap(
-					hash
-				);
-				setEnsName(name)
+			);
+			setHasClaimed(
+				!!web3.utils.toAscii(hash).replaceAll("\u0000", "").length
+			);
+			if (!!web3.utils.toAscii(hash).replaceAll("\u0000", "").length) {
+				const name = await buildContract()?.hashToDomainMap(hash);
+				setEnsName(name);
 			}
 		} catch (error) {
 			console.error(error);
 		} finally {
-			setLoading(false)
+			setLoading(false);
 		}
-	}, [address, isConnected])
+	}, [address, isConnected]);
 
 	useEffect(() => {
-		checkIfAlreadyExists()
-	}, [address, isConnected])
+		checkIfAlreadyExists();
+	}, [address, isConnected]);
 
 	useEffect(() => {
-		if(isDisconnected) setHasClaimed(false)
-	}, [isDisconnected])
-	
-	const shouldShowMsg = (isDisconnected ? true : onWhiteList)
-	console.log(onWhiteList, hasClaimed)
+		if (isDisconnected) setHasClaimed(false);
+	}, [isDisconnected]);
+
+	const shouldShowMsg = isDisconnected ? true : onWhiteList;
+	console.log(onWhiteList, hasClaimed);
 
 	return (
 		<div className="py-6 justify-center text-center bg-dark-blue h-screen flex items-center text-white flex-col">
@@ -106,94 +114,82 @@ const Home: NextPage = () => {
 					</span>
 				</div>
 				<div className="flex space-x-6 items-center">
-					{
-						shouldShowMsg && (
-							<a
-								className="underline md:block hidden"
-								href="onchain.skiptheline.dev"
-								target={"_blank"}
-							>
-								Start your web3 journey today
-							</a>
-						)
-					}
-					{!!isLoginSuccessful && (<div><ConnectButton /></div>)}
+					{shouldShowMsg && (
+						<a
+							className="underline md:block hidden"
+							href="onchain.skiptheline.dev"
+							target={"_blank"}
+						>
+							Start your web3 journey today
+						</a>
+					)}
+					{!!isLoginSuccessful && (
+						<div>
+							<ConnectButton />
+						</div>
+					)}
 				</div>
 			</div>
 			<div className="absolute top-0 left-0 w-screen h-screen decorative-stuff overflow-hidden">
 				<WavesImage classnames="absolute top-0 right-0 decorative-stuff" />
-				<div className="blob decorative-stuff"/>
+				<div className="blob decorative-stuff" />
 			</div>
 			<div className="max-w-4xl mx-auto md:mb-12 px-4 md:px-0">
-				{
-					!shouldShowMsg && (
-						<>
-							<img className="mx-auto mb-8 rounded-3xl" src="https://media4.giphy.com/media/l0ErQ2UfBNFEIlqjC/giphy.gif?cid=ecf05e47f69jrncvqz3b3ttzqco0sp1dxj2iz56bizguntrx&rid=giphy.gif&ct=g" alt="gif" />
-						</>
-					)
-				}
-				{
-					!hasClaimed && (
-						<div className="text-center md:mb-24 mb-12">
-							<h1 className="md:text-6xl text-2xl font-bold leading-13 mb-4">
-								{
-									shouldShowMsg ? (
-										<>
-											Welcome, <br /> Let’s get you a custom <br /> ENS subdomain
-										</>
-									) : (
-										<>Sorry this wallet is not whitelisted</>
-									)
-								}
-							</h1>
-							<p className="md:text-xl text-md">
-								{
-									shouldShowMsg ? (
-										<>
-											You can use this to easily recieve & send tokens in your wallet.
-										</>
-									) : (
-										<>
-											You can use this to easily recieve & send tokens in your wallet.
-										</>
-									)
-								}
-							</p>
-							{
-								!shouldShowMsg && (
-									<a
-										className="underline mt-4 md:block hidden"
-										href="https://onchain.skiptheline.dev"
-										target={"_blank"}
-									>
-										Start your web3 journey today
-									</a>
-								)
-							}
-						</div>
-					)
-				}
+				{!shouldShowMsg && (
+					<>
+						<img
+							className="mx-auto mb-8 rounded-3xl"
+							src="https://media4.giphy.com/media/l0ErQ2UfBNFEIlqjC/giphy.gif?cid=ecf05e47f69jrncvqz3b3ttzqco0sp1dxj2iz56bizguntrx&rid=giphy.gif&ct=g"
+							alt="gif"
+						/>
+					</>
+				)}
+				{!hasClaimed && (
+					<div className="text-center md:mb-24 mb-12">
+						<h1 className="md:text-6xl text-2xl font-bold leading-13 mb-4">
+							{shouldShowMsg ? (
+								<>
+									Welcome, <br /> Let’s get you a custom{" "}
+									<br /> ENS subdomain
+								</>
+							) : (
+								<>Sorry this wallet is not whitelisted</>
+							)}
+						</h1>
+						<p className="md:text-xl text-md">
+							{shouldShowMsg ? (
+								<>
+									You can use this to easily recieve & send
+									tokens in your wallet.
+								</>
+							) : (
+								<>
+									You can use this to easily recieve & send
+									tokens in your wallet.
+								</>
+							)}
+						</p>
+						{!shouldShowMsg && (
+							<a
+								className="underline mt-4 md:block hidden"
+								href="https://onchain.skiptheline.dev"
+								target={"_blank"}
+							>
+								Start your web3 journey today
+							</a>
+						)}
+					</div>
+				)}
 
-				{
-					!isLoginSuccessful ? (
-						<div className="flex justify-center">
-							<BuildConnectButton />
-						</div>
-					) : onWhiteList 
-					? hasClaimed 
-					? (
-						<div className="text-white p-12 flex flex-col space-y-12 m-auto border border-white rounded-3xl w-[700px]">
-							<div className="flex items-center justify-between">
-								<h1 className="font-bold text-4xl text-left">Get your sub-domain of your community!</h1>
-								<img src="/biglogo.png" className=" w-36 h-36" alt="" />
-							</div>
-							<div className="w-full rounded-lg text-left shadow-[7px_5px_0_2px] shadow-[#1649FF] py-8 px-6 bg-white">
-								<p className="text-5xl text-[#1D263B]" >{`${ensName}.isbuildingon.eth`}</p>
-							</div>
-						</div>
-					)
-					: (
-						<form className="">
+				{!isLoginSuccessful ? (
+					<div className="flex justify-center">
+						<BuildConnectButton />
+					</div>
+				) : onWhiteList ? (
+					hasClaimed ? (
+						<ClaimedCard ensName={ensName} />
+					) : (
+						<form className="" onSubmit={claimEns}>
 							{isLoginSuccessful && (
 								<label className="flex space-x-2 items-center border border-white/[.1] focus-within:border-white rounded-2xl py-4 px-4 mb-4 focus-within:bg-white/[.05] transition-all duration-300 backdrop-blur">
 									<input
@@ -202,7 +198,7 @@ const Home: NextPage = () => {
 										className=" border-0 flex-1 bg-transparent block w-full focus:outline-none"
 										value={name}
 										onChange={(e) => {
-											setName(e.target.value)
+											setName(e.target.value);
 										}}
 									/>
 									<span className="text-white">
@@ -210,16 +206,19 @@ const Home: NextPage = () => {
 									</span>
 								</label>
 							)}
-							{
-								!!isLoginSuccessful && (
-									<div className="flex items-center justify-around">
-										<Button loading={loading} onClick={claimEns}>Claim Your ENS!</Button>
-									</div>
-								)
-							}
+							{!!isLoginSuccessful && (
+								<div className="flex items-center justify-around">
+									<Button
+										loading={loading}
+										onClick={claimEns}
+									>
+										Claim Your ENS!
+									</Button>
+								</div>
+							)}
 						</form>
-					) : null
-				}
+					)
+				) : null}
 			</div>
 		</div>
 	);
--- utils/buildContract.ts
+++ utils/buildContract.ts
@@ -1,19 +1,24 @@
 import { ethers, Signer } from "ethers";
-import buildContractABI from '../abis/build.json';
+import buildContractABI from "../abis/build.json";
 
 declare let window: any;
 
 export const buildContract = (walletAddress?: string) => {
-    const {ethereum} = window;
-    if(ethereum) {
-       const provider = new ethers.providers.Web3Provider(ethereum);
-       const signer = provider.getSigner(walletAddress)
-       const contractReader = new ethers.Contract(
-            "0x0A5d2045B999469ED0DcaE2dC58FEc7f83e146f8",
-            buildContractABI,
-            signer
-       )
-       return contractReader
-    }
-    return null;
-}
+	const { ethereum } = window;
+	if (ethereum) {
+		const contractAddress =
+			process.env.NODE_ENV == "development"
+				? "0x7CEbc67ad1f0d0391781bD80cc608369303dab80"
+				: "0x0A5d2045B999469ED0DcaE2dC58FEc7f83e146f8";
+
+		const provider = new ethers.providers.Web3Provider(ethereum);
+		const signer = provider.getSigner(walletAddress);
+		const contractReader = new ethers.Contract(
+			contractAddress,
+			buildContractABI,
+			signer
+		);
+		return contractReader;
+	}
+	return null;
+};
